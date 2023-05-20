4b1a7395f7093cfbf715008ac9d707b27b3c3a3d
sw-n2-9h <chinhnguyennn24@gmail.com>
2023-05-20 09:17:01 +0700

    drag

--- main.js
+++ main.js
@@ -30,10 +30,43 @@ closeMenuBtn.addEventListener('click', function(){
     navHeader.classList.remove('active')
     searchBox.classList.remove('hide')
 })
-
 document.addEventListener('click', function(event) {
     if (!navHeader.contains(event.target) && !menuBtn.contains(event.target)) {
         navHeader.classList.remove('active');
         searchBox.classList.remove('hide')
     }
 });
+
+// Drag / Drop
+const trashItem = document.querySelector('#trash');
+const trashBin = document.querySelector('.trash-bin-box');
+const noneDropF = document.querySelector('.box-trash .img-box');
+const noneDropS = document.querySelector('.box-trash h4');
+const ringLoading = document.querySelector('.lds-ring');
+const dropDoneMess = document.querySelector('.box-trash h3');
+const loaderWr = document.querySelector('#loader');
+
+trashBin.addEventListener('dragover', function(e){
+    e.preventDefault();
+})
+trashItem.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('trash', e.target.id)
+})
+trashBin.addEventListener('drop', e => {
+    e.preventDefault();
+    let data = e.dataTransfer.getData('trash')
+    e.target.appendChild(document.getElementById(data));
+    noneDropF.style.display = 'none';
+    noneDropS.style.display = 'none';
+    ringLoading.style.display = 'block';
+    setTimeout(() => {
+        ringLoading.style.display = 'none';
+        dropDoneMess.style.display = 'block'
+        setTimeout(() => {
+            loaderWr.style.display = 'none';
+        }, 1000)
+    }, 3000)
+})
+
+
+
